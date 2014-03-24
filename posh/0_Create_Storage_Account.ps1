d5e0900f42692e603e56a2b4ea295edc3fde667c
lararubbelke <larar@microsoft.com>
2014-03-23 20:04:13 -0700

    posh scripts document

---
+++ posh/0_Create_Storage_Account.ps1
@@ -0,0 +1,37 @@
+﻿<#############################################################################################################
+Hadoop on Azure Virtual Machines
+
+.SYNOPSIS 
+  Create the storage accounts for Hadoop on Azure deployments on Azure virtual machines.  
+
+.DESCRIPTION 
+  Used to automate the creation of Windows Azure infrastructure to support the deploying Hadoop  
+  on Windows Azure Virtual Machines.  
+
+  Create a single storage accountr.  
+  
+.EXAMPLE 
+  .\0_Create_Storage_Account.ps1 -affinityGroupName "hdpazureAG" -storageAccountName "hdpstorage" 
+
+
+############################################################################################################>
+
+param ( 
+    # Affinity Group of the blob storage account
+    [Parameter(Mandatory = $true)] 
+    [String]$affinityGroupName, 
+     
+    # Blob storage account for storing vhds and scripts 
+    [Parameter(Mandatory = $true)] 
+    [String]$storageAccountName 
+    )      
+
+# Check if account already exists then use it 
+
+$storageAccount = Get-AzureStorageAccount | Where {$_.StorageAccountName -eq $storageAccountName}
+if ($storageAccount -eq $null) { 
+    Write-Verbose "Creating new storage account $storageAccountName." 
+    $storageAccount = New-AzureStorageAccount –StorageAccountName $storageAccountName -AffinityGroup $affinityGroupName 
+} else { 
+    Write-Verbose "Using existing storage account $storageAccountName." 
+} 
---
+++ posh/Management Scripts/99_StopAllVMs.ps1
@@ -0,0 +1,12 @@
+﻿<#############################################################################################################
+Hadoop on Azure Virtual Machines
+
+.SYNOPSIS 
+  Management script to stop all virtual machines follow a naming convention.   
+
+############################################################################################################>
+$vmNamePrefix = "ncdHDPa0"
+
+
+Get-AzureVM | where {$_.Name -like "*$vmNamePrefix"} | Stop-AzureVM -Force
+
